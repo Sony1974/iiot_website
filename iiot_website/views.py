ffbbfb798922ca805ac77c528091c3000d427805
‘Sony1974’ <sony.mathew.thomas@gmail.com>
2022-07-26 20:40:37 +0300

    added templates folder with home.html

--- iiot_website/views.py
+++ iiot_website/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+# def home(request):
+#     return HttpResponse('Welcome to IIoT Startup Project')
 
 def home(request):
-    return HttpResponse('Welcome to IIoT Startup Project')
+    return render(request,'home.html')
