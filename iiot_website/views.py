49093215a2c4608f5ac37069a85183ba7ff37e56
‘Sony1974’ <sony.mathew.thomas@gmail.com>
2022-07-26 21:05:32 +0300

    model objects fields dispalyed in template file

--- iiot_website/views.py
+++ iiot_website/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from notifications.models import Notifications
+
 
 # def home(request):
 #     return HttpResponse('Welcome to IIoT Startup Project')
 
 def home(request):
-    return render(request,'home.html')
+    notif = Notifications.objects.all()
+
+    context = { 
+        'notif' : notif
+    }
+    return render(request,'home.html', context)
