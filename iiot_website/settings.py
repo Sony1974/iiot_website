95719baecf2c429e182370bb482d897f5eee0901
‘Sony1974’ <sony.mathew.thomas@gmail.com>
2022-07-26 19:38:17 +0300

    added new staging website address to ALLOWED HOST

--- iiot_website/settings.py
+++ iiot_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-irn4c@zp9il_*!lcao01jod8$m#^d(n6t!0umiu^@2v*mxd#f*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['IIoT-Startup-dev.us-west-2.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
