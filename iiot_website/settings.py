f7ae9d5833cedd310b94a9fa9342ce1333254993
‘Sony1974’ <sony.mathew.thomas@gmail.com>
2022-07-27 15:11:51 +0300

    removed last changes in database security due to deployment failure

--- iiot_website/settings.py
+++ iiot_website/settings.py
@@ -113,11 +113,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': config('DATABASE_NAME'),
-            'USER': config('DATABASE_USER'),
-            'PASSWORD': config('DATABASE_PASSWORD'),
-            'HOST': config('DATABASE_HOST'),
-            'PORT': config('DATABASE_PORT'),
+            'NAME': 'iiot_startup',
+            'USER': 'postgres',
+            'PASSWORD': '80170174',
+            'HOST': 'localhost',
+            'PORT': '5432',
         }
     }
 
