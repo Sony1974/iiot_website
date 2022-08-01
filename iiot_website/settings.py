7a663798c2e357b6b257c7ed7d490910db83faa5
‘Sony1974’ <sony.mathew.thomas@gmail.com>
2022-08-01 14:08:25 +0300

    DEBUG ststus change with decouple

--- iiot_website/settings.py
+++ iiot_website/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')  # Python decouple version 
+SECRET_KEY = config('SECRET_KEY')  # Python decouple version worked after updating 
 #SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','<replace me with your own fallback secret key>')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = config('DEBUG', default=True, cast=bool) # True
-DEBUG = os.environ.get('DJANGO_DEBUG', '1').lower() in ['true', 't', '1']
+DEBUG = config('DEBUG', default=True, cast=bool) # True
+#DEBUG = os.environ.get('DJANGO_DEBUG', '1').lower() in ['true', 't', '1']
 
 
 ALLOWED_HOSTS = ['IIoT-Startup-dev.us-west-2.elasticbeanstalk.com','127.0.0.1','scciiot.com','*']
