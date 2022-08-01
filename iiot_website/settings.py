1d30cc7447e6277cbbc7aa1aa9403aa8bf46843e
‘Sony1974’ <sony.mathew.thomas@gmail.com>
2022-08-01 15:19:25 +0300

    check static update working with new code in 01_django.config

--- iiot_website/settings.py
+++ iiot_website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')  # Python decouple version worked after updating 
+SECRET_KEY = config('SECRET_KEY')  # Python decouple version worked after updat
 #SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','<replace me with your own fallback secret key>')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -169,44 +169,44 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # ================================================================================
 # As we are moving to AWS S3 bucket we comment this section now
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIRS = [ 
-    'iiot_website/static',
-]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR /'static'
+# STATICFILES_DIRS = [ 
+#     'iiot_website/static',
+# ]
 
 # ===============================================================================
 # Static file configuration for AWS S3 Bucket - From now the Django project will access static files directlty from AWS S3 bucket with below setup
 # AWS S3 Static Files Configuration
 
 
-# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = 'public-read'
-# AWS_LOCATION = 'static'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
 
-# STATICFILES_DIRS = [
-#     'iiot_website/static',
-# ]
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_DIRS = [
+    'iiot_website/static',
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Default media storgae files location
 
 
-# DEFAULT_FILE_STORAGE = 'iiot_website.media_storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'iiot_website.media_storages.MediaStorage'
 
 # media files configuration
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 
 
